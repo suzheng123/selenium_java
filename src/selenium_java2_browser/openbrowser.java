a0081ecd618cc85a456494f5c3d3f1b7a78c4f9f
suzheng123 <suzheng@ucsc.edu>
2019-10-16 23:02:23 -0700

    commit
    second commit

---
+++ src/selenium_java2_browser/openbrowser.java
@@ -0,0 +1,31 @@
+package selenium_java2_browser;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class openbrowser {
+	
+	public static WebDriver driver = null;
+	
+	public static void main(String[] args) {
+		
+//		System.setProperty("webdreiver.chrome.driver", "/Users/JacZh/desktop/resources/selenium_java/selenium_java2/drivers");
+//		driver = new ChromeDriver();
+		System.setProperty("webdreiver.firefox.driver", "/Users/JacZh/desktop/resources/selenium_java/selenium_java2/drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+		
+		String baseUrl="https://www.amazon.com";
+		driver.get(baseUrl);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+	
+		String title = driver.getTitle();
+		
+		System.out.println(title);
+	}
+
+}
